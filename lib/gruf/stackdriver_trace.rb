5605dff21b68d659d244fc5cc9dad7e2601b4305
Kei Takahashi <dameleon@gmail.com>
2019-11-28 21:40:21 +0900

    add constants

--- lib/gruf/stackdriver_trace.rb
+++ lib/gruf/stackdriver_trace.rb
@@ -2,5 +2,7 @@ require "gruf/stackdriver_trace/version"
 
 module Gruf
   module StackdriverTrace
+    AGENT_NAME = "gruf-stackdriver-trace #{Gruf::StackdriverTrace::VERSION}".freeze
+    HEADER_KEY = 'x-trace-context'.freeze
   end
 end
