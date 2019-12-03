cbc549342eb584404b842af69300a7a7ba8ff319
Kei Takahashi <dameleon@gmail.com>
2019-12-03 11:25:23 +0900

    fix require path

--- lib/gruf/stackdriver_trace.rb
+++ lib/gruf/stackdriver_trace.rb
@@ -1,4 +1,6 @@
-require "gruf/stackdriver_trace/version"
+require_relative "stackdriver_trace/client_interceptor"
+require_relative "stackdriver_trace/server_interceptor"
+require_relative "stackdriver_trace/version"
 
 module Gruf
   module StackdriverTrace
